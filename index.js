18858e5d7a17e16c31333662c1f8437af782cc50
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-25 11:30:17 +1100

    Added note regarding partial implementation status

--- index.js
+++ index.js
@@ -6,6 +6,17 @@
   providing a custom SIP.js `mediaHandlerFactory` that can be used to override
   the default WebRTC behaviour of SIP.js
 
+  ## NOTE: Incomplete
+
+  This package has been implemented to the point that it is usable for the
+  following purpose:
+
+  - Using a SIP endpoint to attach to a WebRTC call for the purposes of
+    recording audio.
+
+  While other features will be implemented over time, these is not as high
+  a priority as other implementation tasks within the rtc.io suite.
+
   ## Example Usage
 
   To be completed.

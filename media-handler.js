c693ff86108df36b64b4afbc5f65826e5dfd9a34
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-25 11:28:26 +1100

    Complete implementation of media-handler to the point that it is usable for recording

--- media-handler.js
+++ media-handler.js
@@ -3,6 +3,8 @@ var inherits = require('inherits');
 var capture = require('rtc-capture');
 var utils = require('./utils');
 var taskqueue = require('rtc-taskqueue');
+var ObservIce = require('observ-ice');
+var SIP = require('sip.js');
 
 function MediaHandler(config, session, opts) {
   if (! (this instanceof MediaHandler)) {
@@ -17,6 +19,7 @@ function MediaHandler(config, session, opts) {
   // create a peer connection
   this.pc = utils.createPeer(config, session, opts);
   this.queue = taskqueue(this.pc, config);
+  this.ice = ObservIce(this.pc);
 }
 
 inherits(MediaHandler, EventEmitter);
@@ -31,26 +34,81 @@ prot.close = function() {
 prot.getDescription = function(succeed, fail, hint) {
   var pc = this.pc;
   var q = this.queue;
+  var ice = this.ice;
+
+  // generate some default constraints if none supplied
+  var constraints = (hint || {}).constraints || { audio: true, video: true };
 
   function connect(stream) {
-    var methodName = pc.signalingState === 'have-remote-offer' ? 'createAnswer' : 'createOffer';
+    var methodName = utils.hasOffer(pc, 'remote') ? 'createAnswer' : 'createOffer';
+    var stop;
 
-    q.once('sdp.local', succeed);
+    q.once('sdp.local', function(desc) {
+      if (ice.gathered()) {
+        return ready(pc.localDescription.sdp);
+      }
+      else {
+        stop = ice.gathered(function(value) {
+          if (value) {
+            stop();
+            ready(pc.localDescription.sdp);
+          }
+        });
+      }
+    });
 
     pc.addStream(stream);
     q[methodName].call(q);
   }
 
+  function ready(sdp) {
+//     sdp = SIP.Hacks.Chrome.needsExplicitlyInactiveSDP(sdp);
+//     sdp = SIP.Hacks.AllBrowsers.unmaskDtls(sdp);
+
+    succeed(sdp);
+  }
+
   console.log('getDescription called: ', arguments);
-  if (hint.stream) {
+  if (hint && hint.stream) {
     return connect(hint.stream);
   }
 
-  capture(hint.constraints, this.config, function(err, stream) {
+  capture(constraints, this.config, function(err, stream) {
     if (err) {
       return fail(err);
     }
 
     return connect(stream);
   });
+};
+
+prot.setDescription = function(sdp, pass, fail) {
+  var raw = {
+    type: utils.hasOffer(this.pc, 'local') ? 'answer' : 'offer',
+    sdp: sdp
+  };
+
+  this.pc.setRemoteDescription(utils.createSessionDesc(this.config, raw), pass, fail);
+};
+
+// TODO: implement these additional functions
+prot.isMuted = function() {
+  return {
+    audio: false,
+    video: false
+  };
+};
+
+prot.mute = function(opts) {};
+prot.unmute = function(opts) {};
+
+prot.hold = function() {};
+prot.unhold = function() {};
+
+prot.getLocalStreams = function() {
+  return this.pc.getLocalStreams();
+};
+
+prot.getRemoteStreams = function() {
+  return this.pc.getRemoteStreams();
 };
